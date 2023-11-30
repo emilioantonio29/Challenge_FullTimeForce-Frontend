18e2b72c49e7613750ce4299ec47f09e1c35df0e
emilioantonio29 <emilio_git@outlook.com>
2023-11-29 22:06:33 -0300

    main structure completed

---
+++ src/components/common/main-loader/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BiSolidBank} from 'react-icons/bi'
+import logo from '../../../assets/fulltimeforce.png'
+
+const CommonMainLoader = () => {
+    return (
+        <>
+            <div className='d-flex flex-column justify-content-center align-items-center' style={{height: "100vh"}}>
+                <div className='d-flex justify-content-center'>
+                    <img src={logo}  alt="Logo" height="150"/>
+                    {/* <BiSolidBank size='100px' style={{color: "#000d4e"}}/> */}
+                </div>
+                <div className='d-flex justify-content-center align-items-center' style={{marginTop: "15px"}}>
+                    <div style={{width: "1rem", height: "1rem", color: "#B88CB8"}} className="spinner-grow text-muted"></div> 
+                    <div style={{marginLeft: "5px", width: "1rem", height: "1rem", color: "#B88CB8"}}className="spinner-grow text-muted"></div>
+                    <div style={{marginLeft: "5px", width: "1rem", height: "1rem", color: "#B88CB8"}}className="spinner-grow text-muted"></div>
+                </div>
+            </div>
+        </>
+
+    );
+}
+export default CommonMainLoader;
